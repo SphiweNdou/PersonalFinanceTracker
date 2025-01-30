4671e5941f75032d765b34330d5f7ba6374cb194
Sphiwe Ndou <ndebondou@gmail.com>
2025-01-30 17:37:33 +0200

    Add JWT authentication Controller

---
+++ PersonalFinanceTracker/Controllers/AuthController.cs
@@ -0,0 +1,90 @@
+﻿namespace PersonalFinanceTracker.Controllers
+{
+    using System;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+    using System.Text;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.IdentityModel.Tokens;
+    using PersonalFinanceTracker.Models;
+
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest registerRequest)
+        {
+            var user = new ApplicationUser
+            {
+                FullName = registerRequest.FullName,
+                Email = registerRequest.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, registerRequest.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok(new
+                {
+                    Message = "User registered succesfully!"
+                });
+            }
+
+            return BadRequest(result.Errors);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+        {
+            var user = await _userManager.FindByEmailAsync(loginRequest.Username);
+
+            if(user == null || !await _userManager.CheckPasswordAsync(user,loginRequest.Password))
+            {
+                return Unauthorized(new
+                {
+                    Message = "Invalid username or password."
+                });
+            }
+
+            var token = GenerateJwtToken(user);
+            return Ok(new
+            {
+                Token = token
+            });
+        }
+
+        private string GenerateJwtToken(ApplicationUser user)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["jwt:key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["jwt:issuer"],
+                audience: _configuration["jwt:Audience"],
+                claims: claims, expires: DateTime.UtcNow.AddHours(1));
+            
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
