c84ee9eae49c63ddbb84dd8129249bffd0694067
Sphiwe Ndou <ndebondou@gmail.com>
2025-01-30 16:48:13 +0200

    Configure jwt authentication Add validation parameter to appsettings

--- PersonalFinanceTracker/Program.cs
+++ PersonalFinanceTracker/Program.cs
@@ -32,6 +32,26 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 //identity Configuration
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
+//JWT Authentication Configuration
+var jwtSettings = builder.Configuration.GetSection("jwt");
+var key = Encoding.UTF8.GetBytes(jwtSettings["Key"]);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(key)
+    };
+});
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
