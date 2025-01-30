052a87f76c6ee3bda78d07e8890c49a197f92898
Sphiwe Ndou <ndebondou@gmail.com>
2025-01-30 15:04:26 +0200

    Configure Swagger Setup

--- PersonalFinanceTracker/Program.cs
+++ PersonalFinanceTracker/Program.cs
@@ -1,3 +1,9 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +11,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen( options => 
+{
+    options.SwaggerDoc("v1", new OpenApiInfo 
+    {
+        Title = "Personal Finace Tracker API",
+        Version = "v1"
+    });
+}
+);
 
 var app = builder.Build();
 
