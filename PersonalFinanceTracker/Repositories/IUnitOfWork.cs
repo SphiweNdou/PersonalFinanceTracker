507ebe3702c2ec700702d71cb1db21a73a5463af
Sphiwe Ndou <ndebondou@gmail.com>
2025-01-31 10:41:09 +0200

    Add unit of work interface

---
+++ PersonalFinanceTracker/Repositories/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using PersonalFinanceTracker.Models;
+
+namespace PersonalFinanceTracker.Repositories
+{
+    public interface IUnitOfWork
+    {
+        ITransactionRepository Transactions { get; }
+        IRepository<ApplicationUser> Users { get; }
+        Task<int> CompleteAsync();
+    }
+}
