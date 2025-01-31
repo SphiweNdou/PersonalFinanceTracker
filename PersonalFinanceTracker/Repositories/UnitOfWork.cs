1fac64490bdd60a06578e56af2b48fc60efc947e
Sphiwe Ndou <ndebondou@gmail.com>
2025-01-31 10:45:21 +0200

    Add Unit of Work Repository

---
+++ PersonalFinanceTracker/Repositories/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿namespace PersonalFinanceTracker.Repositories
+{
+    using PersonalFinanceTracker.Data;
+    using PersonalFinanceTracker.Models;
+
+    public class UnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public ITransactionRepository Transactions { get; }
+        public IRepository<ApplicationUser> Users { get; }
+        public UnitOfWork(ApplicationDbContext context, ITransactionRepository transactions, IRepository<ApplicationUser> users)
+        {
+            _context = context;
+            Transactions = transactions;
+            Users = users;
+        }
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
