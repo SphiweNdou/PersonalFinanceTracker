6dfa5512a329bb9ae3ece175af4785f56465665a
Sphiwe Ndou <ndebondou@gmail.com>
2025-01-31 12:24:28 +0200

    Update migrations to updated model

--- PersonalFinanceTracker/Migrations/ApplicationDbContextModelSnapshot.cs
+++ PersonalFinanceTracker/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,7 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using PersonalFinanceTracker.Models;
+using PersonalFinanceTracker.Data;
+
 
 #nullable disable
 
