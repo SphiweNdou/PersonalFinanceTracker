597ae043400fb33413bb5313b4754fd7d2cceb11
Sphiwe Ndou <ndebondou@gmail.com>
2025-01-31 08:51:57 +0200

    Add Generic repository interface

---
+++ PersonalFinanceTracker/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace PersonalFinanceTracker.Repositories
+{
+    using System.Linq.Expressions;
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+
+        /*
+         * Query any model 'T' with predicate being the 'where' clause
+         * Expression is used so that the linq 'predicate' can be converted into sql for entity framework
+         */
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        Task Update(T entity);
+        Task Remove(T entity);  
+    }
+}
