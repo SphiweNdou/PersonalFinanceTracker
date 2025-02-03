96b1fff9bfceee25c800c38de88b46cf21e1bdb3
Sphiwe Ndou <sphiwe.ndou@siemens.com>
2025-02-03 10:10:55 +0200

    Added Account Service

---
+++ PersonalFinanceTracker/Services/AccountService.cs
@@ -0,0 +1,39 @@
+﻿using PersonalFinanceTracker.Models;
+using PersonalFinanceTracker.Repositories;
+
+namespace PersonalFinanceTracker.Services
+{
+    public class AccountService : IAccountServices
+    {
+        private readonly IAccountRepository _accountRepository;
+        public AccountService (IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public async Task<IEnumerable<Account>> GetAllAccountsAsync()
+        {
+            return await _accountRepository.GetAllAsync(); 
+        }
+
+        public async Task<Account> GetAccountByIdAsync(int id)
+        {
+            return await _accountRepository.GetByIdAsync(id);
+        }
+
+        public async Task CreateAccountAsync(Account account)
+        {
+            _accountRepository.AddAsync(account);
+        }
+
+        public async Task UpdateAccountAsync(Account account)
+        {
+            _accountRepository.UpdateAsync(account);
+        }
+
+        public async Task DeleteAccountAsync(int id)
+        {
+            await _accountRepository.DeleteAsync(id);
+        }
+    }
+}
---
+++ PersonalFinanceTracker/Services/IAccountServices.cs
@@ -0,0 +1,14 @@
+﻿using PersonalFinanceTracker.Models;
+
+namespace PersonalFinanceTracker.Services
+{
+    public interface IAccountServices
+    {
+        Task<IEnumerable<Account>> GertAllAcountsAsync();
+        Task<Account> GetAccountByIdAsync(int id);
+        Task CreateAccountAsync(Account account);
+        Task UpdateAccountAsync(Account account);
+        Task DeleteAccountAsync(Account account);
+        
+    }
+}
